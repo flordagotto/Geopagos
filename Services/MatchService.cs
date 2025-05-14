28ec9e46c95a2181e545439ad2de45da0d0ff097
Dagotto, Florencia Agustina <68953960+flordagotto@users.noreply.github.com>
2025-05-14 15:16:45 -0300

    create services and dtos (#2)

---
+++ Services/MatchService.cs
@@ -0,0 +1,26 @@
+﻿using DTOs;
+
+namespace Services
+{
+    public interface IMatchService
+    {
+        Task<IEnumerable<PlayerDTO>> GetAll();
+
+        Task Create(MatchDTO match);
+    }
+
+    public class MatchService : IMatchService
+    {
+        public MatchService() { }
+
+        public Task Create(MatchDTO match)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<PlayerDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/PlayerService.cs
@@ -0,0 +1,26 @@
+﻿using DTOs;
+
+namespace Services
+{
+    public interface IPlayerService
+    {
+        Task<IEnumerable<PlayerDTO>> GetAll();
+
+        Task Create(PlayerDTO player);
+    }
+
+    public class PlayerService : IPlayerService
+    {
+        public PlayerService() { }
+
+        public Task Create(PlayerDTO player)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<PlayerDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
