d684f260d48e6947bc9b2c67efb3d24365481566
Dagotto, Florencia Agustina <68953960+flordagotto@users.noreply.github.com>
2025-05-14 14:52:47 -0300

    Domain logic (#1)
    * create player object domain
    
    * create tournament and match
    
    * finish domain logic

---
+++ Domain/Entities/Player.cs
@@ -0,0 +1,69 @@
+﻿using Common.Enums;
+
+namespace Domain.Entities
+{
+    public abstract class Player : Entity
+    {
+        protected Player(string name, int skill, Gender gender)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name is required.");
+
+            if (skill < 0 || skill > 100)
+                throw new ArgumentException("Skill must be between 0 and 100.");
+
+            Name = name;
+            Skill = skill;
+            Gender = gender;
+        }
+
+        public string Name { get; }
+
+        public int Skill { get; }
+
+        public Gender Gender { get; }
+    }
+
+    public class FemalePlayer : Player
+    {
+        private FemalePlayer(string name, int skill, int reactionTime) : base(name, skill, Gender.Female)
+        {
+            ReactionTime = reactionTime;
+        }
+
+        public static FemalePlayer Create(string name, int skill, int reactionTime)
+        {
+            if (reactionTime < 0)
+                throw new ArgumentException("Reaction time must be greater than 0.");
+
+            return new FemalePlayer(name, skill, reactionTime);
+        }
+
+        public int ReactionTime { get; }
+    }
+
+
+    public class MalePlayer : Player
+    {
+        private MalePlayer(string name, int skill, int strength, int speed) : base(name, skill, Gender.Male)
+        {
+            Strength = strength;
+            Speed = speed;
+        }
+
+        public static MalePlayer Create(string name, int skill, int strength, int speed)
+        {
+            if (strength < 0)
+                throw new ArgumentException("Strength must be greater than 0.");
+
+            if (speed < 0)
+                throw new ArgumentException("Speed must be greater than 0.");
+
+            return new MalePlayer(name, skill, strength, speed);
+        }
+
+        public int Strength { get; }
+
+        public int Speed { get; }
+    }
+}
---
+++ Domain/Entities/Tournament.cs
@@ -0,0 +1,42 @@
+﻿using Common.Enums;
+
+namespace Domain.Entities
+{
+    public class Tournament : Entity
+    {
+        public Gender Type { get; }
+
+        public DateTime Created { get; }
+
+        public bool IsFinished { get; private set; }
+
+        public Guid WinnerId { get; private set; }
+
+        public Player? Winner { get; private set; }
+
+        public List<Match>? Matches { get; set; }
+
+
+        private readonly List<Player> _players;
+        public IReadOnlyCollection<Player> Players => _players.AsReadOnly();
+
+
+        private Tournament(Gender type, IEnumerable<Player> players)
+        {
+            Type = type;
+            Created = DateTime.Now;
+            IsFinished = false;
+            _players = [.. players];
+        }
+
+        public Tournament Create(Gender type, IEnumerable<Player> players)
+        {
+            if(players == null || !players.Any()) 
+                throw new ArgumentNullException("A tournament should have at least 2 players.");
+
+            return new Tournament(type, players);
+        }
+
+        public bool HasPlayer(Player player) => _players.Contains(player);
+    }
+}
