1b3f50c439ad99e158ae1efb477f773466b8a64a
florencia dagotto <flopydagotto@gmail.com>
2025-05-15 15:11:00 -0300

    add validation of amount of players

--- Domain/Entities/Tournament.cs
+++ Domain/Entities/Tournament.cs
@@ -34,6 +34,9 @@ namespace Domain.Entities
             if(players == null || !players.Any() || players.Count() < 2) 
                 throw new ArgumentNullException("A tournament should have at least 2 players.");
 
+            if (!IsPowerOfTwo(players.Count()))
+                throw new ArgumentNullException("The amount of players in a tournament should be power of two");
+
             if (players.Any(p => p.Gender != type))
                 throw new ArgumentException("All players must have the same gender as the tournament.");
 
@@ -41,5 +44,10 @@ namespace Domain.Entities
         }
 
         public bool HasPlayer(Player player) => _players.Contains(player);
+
+        private static bool IsPowerOfTwo(int number)
+        {
+            return number > 0 && (number & (number - 1)) == 0;
+        }
     }
 }
